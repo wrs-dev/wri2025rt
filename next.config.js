9399b5cbf89a4a28605ca1d91593393b541af912
Stephen Parker <sparker888@gmail.com>
2024-09-12 17:49:03 -0400

    update netlify.toml and next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["a-us.storyblok.com", "interfacejournal.com"], 
+    domains: ["a-us.storyblok.com", "interfacejournal.com"],
+    unoptimized: true,
+  },
+  output: 'export',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
   },
 };
 
